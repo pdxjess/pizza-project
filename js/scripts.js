8467b215ac8db6fefb22abb22229183d110f4e59
Jessica Doss <doss.jessicam@gmail.com>
2019-06-21 12:48:41 -0700

    and functionality to js for ordering totals to work

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ function Pizza(size,toppings) {
   this.size = size
   this.toppings = toppings
 }
-
 Pizza.prototype.priceCalculate = function(price, toppings) {
   var price = 0;
   if (this.size !== 1 && this.size !== 2 && this.size !==3) {
@@ -18,9 +17,26 @@ Pizza.prototype.priceCalculate = function(price, toppings) {
   if (this.size === 3) {
     price += 24;
   }
-  for (var i=0; 1 < this.toppings.length; i ++) {
+  for (var i=0; i < this.toppings.length; i ++) {
     price += 2;
   }
   this.price = price;
   return price;
 }
+var userToppings = [];
+$(document).ready(function(){
+  $("form#pizzaMenu").submit(function(event) {
+    event.preventDefault();
+    var userSize = parseInt($("input:checkbox[name=size]:checked").val());
+    $("input:checkbox[name=topping]:checked").each(function(){
+      userToppings.push($(this).val())
+      debugger
+    })
+    var newPizza = new Pizza(userSize, userToppings);
+    var pizzaPrice = newPizza.priceCalculate()
+    $("#showPrice").show().html("Your total is:" + "$" + pizzaPrice + "<br><a href=order.html> Place another order</a>")
+    $('input[type="checkbox"]').prop('checked', false);
+    $('input[type="checkbox"]').prop('checked', false);
+    $("#pizzaMenu").find(':submit').attr('disabled','disabled');
+  });
+});
